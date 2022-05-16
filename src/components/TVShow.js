fad41f9619fb9e796dfc885f030a909e6e445adf
NicMortelliti <npmortelliti@gmail.com>
2022-05-15 21:25:35 -0700

    Populate 'alt' field of image so we know what movie SHOULD be shown in each space

--- src/components/TVShow.js
+++ src/components/TVShow.js
@@ -4,7 +4,11 @@ function tvShow(props) {
   return (
     <div>
       <br />
-      <img src={props.show.medium} onClick={props.selectShow} alt="" />
+      <img
+        src={props.show.medium}
+        onClick={props.selectShow}
+        alt={props.show.name}
+      />
     </div>
   );
 }
